8f4358c7f432d0a7cb977dc2280cb8e03ace41ef
Pranit Gaikwad <gaikwadpranit251@gmail.com>
2026-09-24 18:05:09 +0530

    Fix xacro executable substitution in robot launch

--- testbed_description/launch/robot_description.launch.py
+++ testbed_description/launch/robot_description.launch.py
@@ -25,3 +25,32 @@ def generate_launch_description():
             output='screen',
             parameters=[robot_description]),
     ])
+#!/usr/bin/env python3
+
+from launch import LaunchDescription
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    robot_description_content = Command([
+        FindExecutable(name='xacro'),
+        ' ',
+        PathJoinSubstitution([
+            FindPackageShare('testbed_description'),
+            'urdf',
+            'testbed.xacro',
+        ]),
+    ])
+
+    robot_description = {'robot_description': robot_description_content}
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[robot_description],
+        ),
+    ])
